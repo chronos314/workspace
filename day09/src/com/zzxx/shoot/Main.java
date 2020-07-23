de72cb66063013c784c2d45c692558a01b5f2f06
管盛凯 <985803015@qq.com>
2020-07-23 14:34:05 +0800

    第二次提交shoot

--- day09/src/com/zzxx/shoot/Main.java
+++ day09/src/com/zzxx/shoot/Main.java
@@ -12,7 +12,7 @@ import java.util.TimerTask;
 
 public class Main extends JPanel{
     private int flyingIndex=0;//控制频率的
-    private int grade=0;//分数
+    private int grade=0;//记分数
 
     public static BufferedImage hero0;
     public static BufferedImage hero1;
